203e0bf949de9a49726c1e43ae479b18dd2f6d30
Mauricio S <mauricio-sevilla@outlook.com>
2024-01-24 10:15:05 -0600

    Arquitectura del proyecto Agro

---
+++ Agro/Agro/Agro.web/Data/Entities/City.cs
@@ -0,0 +1,11 @@
+﻿namespace Agro.web.Data.Entities
+{
+    public class City
+
+    {
+        //creado por Mauricio Sevilla Madrid
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+    }
+}
